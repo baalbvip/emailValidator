f7bec6d49226950c06dfbdf43a34362d96588e7d
Baalbvip <baalbvip@gmail.com>
2023-03-28 15:46:27 -0400

    new update

--- server.js
+++ server.js
@@ -1,30 +1,84 @@
-const bodyParser = require("body-parser");
-const validator = require("deep-email-validator");
-const express = require("express");
+import deepValidator from "deep-email-validator"
+import express, { response } from "express"
+import dns from "dns"
+
+const app = express();
+app.use(express.json())
+
+app.get("/validate/:email", async (req, res) => {
+    const { email } = req.params;
+    let domain = email.split("@");
+    domain = domain[1];
+
+    let maxTimeout = 50000;
+    let responseTimeOut = 0
+    let verified = { valid: false, verified_type: 'random', msg: "err", intent: 0 }
 
 
-const app = express()
+    responseTimeOut = setTimeout(() => {
+        verified.msg = "Timeout"
+        verified.timeout = true;
+        res.send(verified)
+    }, maxTimeout)
 
-app.use(bodyParser.json())
+    let smtp, verifiedDomain;
+
+    // verifica que el dominio sea verdadero
+    await new Promise((resolve, reject) => {
+        dns.lookup(domain, (err, address, family) => {
+            if (err) {
+                verifiedDomain = false
+            } else {
+                verifiedDomain = true
+            }
+
+            resolve("f");
+        })
+    })
+
+
+    if (verifiedDomain) {
+        // verificame que el mx sea verdadero y tenga registro de smtp
+        smtp = await new Promise((resolve, reject) => {
+            return dns.resolveMx("wildreds.es", (err, address) => {
+                resolve(address[0].exchange);
+            });
+        })
+    }
 
 
 
+    if (verifiedDomain !== undefined && verifiedDomain !== false) {
+        if (smtp !== undefined) {
 
-app.get("/", (req,res) => {
-    res.send("<p>What are you doing?</p>")
+            // Pasa por el validador mas estricto, si este devuelve false y los mensajes son raros pasemos por otro validador
+            verified.valid = deepValidator.validate(email).then((result) => {
+                if (result.valid) {
+                    let validators = result.valid.validators
+
+                    if (validators.regex.valid) {
+                        if (validators.smtp.valid) {
+                            return true;
+                        }
+                    }
+                }
+
+                if (!verified.valid) {
+                    verified.msg = result.reason
+                }
+            })
+
+        }
+    } else {
+        verified.msg = "Domain invalid or Smtp invalid"
+    }
+
+    if (verified.msg !== "Timeout") {
+        clearTimeout(responseTimeOut)
+        res.send(verified);
+    }
 })
 
-
-app.get("/validate/:email", async (req,res) => {
-
-    const {email} = req.params
-    
-   let response = await validator.validate(email)
-
-    res.send(response)
+app.listen(4000, () => {
+    console.log("Reproduciendo en el puerto 4000");
 })
-
-app.listen("4000", () => {
-    console.log("Listen in port 4000")
-})
-
